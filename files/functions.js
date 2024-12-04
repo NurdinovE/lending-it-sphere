b76fbed0fcbbb7c89e0fa3f978126de28eac9bc1
Ver_s <aleksandra.bystrova@alatoo.edu.kg>
2024-12-04 17:02:15 +0600

    clean code, improved structure

--- files/functions.js
+++ files/functions.js
@@ -24,3 +24,73 @@ const navbar = document.querySelector('.nav');
         lastScrollY = currentScrollY <= 0 ? 0 : currentScrollY;
     });
 ////////////////////////////////////////////////////
+//
+//
+//
+//
+/////////////////////////  SCROLL TO LEFT OR RIGHT BY ARROWS
+
+const leftArrow = document.querySelector('.experts__arrow-left');
+const rightArrow = document.querySelector('.experts__arrow-right');
+const cards = document.querySelectorAll('.experts__person');
+const visibleCardsCount = 4; 
+const cardWidth = 280;
+
+let currentIndex = 0; 
+
+function updateVisibleCards() {
+    cards.forEach((card, index) => {
+        if (index >= currentIndex && index < currentIndex + visibleCardsCount) {
+            card.style.transform = `translateX(${(index - currentIndex) * (cardWidth / 6)}px)`; 
+        }
+    });
+}
+
+updateVisibleCards();
+
+rightArrow.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % cards.length;
+    updateVisibleCards();
+});
+
+leftArrow.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+    updateVisibleCards();
+});
+////////////////////////////////////////////////////
+// 
+// 
+// 
+// 
+//////////////////// TO SLIDE QUOTES IN "WHAT CUSTOMERS SAY" SECTION
+document.addEventListener('DOMContentLoaded', () => {
+    const contentWrappers = document.querySelectorAll('.feedback__section2-content-wrapper');
+    const prevButton = document.getElementById('prev');
+    const nextButton = document.getElementById('next');
+    let currentIndex = 0;
+
+    function showSlide(index) {
+        contentWrappers.forEach(wrapper => {
+            wrapper.classList.remove('active', 'previous');
+        });
+
+        contentWrappers[index].classList.add('active');
+    }
+
+    nextButton.addEventListener('click', () => {
+        contentWrappers[currentIndex].classList.add('previous');
+        
+        currentIndex = (currentIndex + 1) % contentWrappers.length;
+        
+        showSlide(currentIndex);
+    });
+
+    prevButton.addEventListener('click', () => {
+        contentWrappers[currentIndex].classList.add('previous');
+        
+        currentIndex = (currentIndex - 1 + contentWrappers.length) % contentWrappers.length;
+        
+        showSlide(currentIndex);
+    });
+});
+////////////////////////////////////////////////////
